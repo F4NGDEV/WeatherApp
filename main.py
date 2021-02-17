6ccd7a026f5408557d305c9389d5f796c06fef9b
Fang <75499121+F4NGDEV@users.noreply.github.com>
2021-02-17 12:31:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import configure
+
+location = input("Location : ")
+
+response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={configure.TOKEN}")
+
+
+if response:
+  response = response.json()
+
+  print(f"""
+
+Weather in {location.title()}
+
+{response["weather"][0]["main"]}
+
+Temprature - {round(response["main"]["temp"] - 273.15 )}° Celcius
+Feels Like - {round(response["main"]["feels_like"] - 273.15 )}° Celcius
+Humidity - {response["main"]["humidity"]} %
+Pressure -  {response["main"]["pressure"]} hPa
+
+  
+  """)
+
+else:
+  print("Location Invalid")
